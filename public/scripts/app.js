169acbc5fcb8b74061fa80d3c916b99bc297e688
Henrique Honorio da Silva <contato@henriquesilva.dev>
2025-11-12 08:47:58 -0300

    feat: update cardapio

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -192,9 +192,9 @@ async function carregarMenu() {
 // ================= [PROMO-REFRIxQTY] Helpers =================
 const PROMO_BEBIDAS = {
   // 'coca-zero-200': 'Coca-Cola Zero 200ml',
-  'fanta-200':     'Fanta 200ml',
+  // 'fanta-200':     'Fanta 200ml',
   'guarana-200':   'Guaraná 200ml',
-  'sukita-200':    'Sukita 200ml',
+  // 'sukita-200':    'Sukita 200ml',
   'pepsi-200':     'Pepsi 200ml'
 };
 
